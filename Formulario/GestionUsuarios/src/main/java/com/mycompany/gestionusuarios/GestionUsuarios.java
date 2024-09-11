719dd0e1bf57abe5e3450a5f4f30512900ce1f47
JosePerez04 <josedavidcogolloperez13@gmail.com>
2024-09-11 14:58:08 -0500

    Add files via upload

---
+++ Formulario/GestionUsuarios/src/main/java/com/mycompany/gestionusuarios/GestionUsuarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionusuarios;
+
+/**
+ *
+ * @author Jose David Perez
+ */
+public class GestionUsuarios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
