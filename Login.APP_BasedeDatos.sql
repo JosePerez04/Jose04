73ab2db266ef6893bd94cbda25d3e276363cb6a8
JosePerez04 <josedavidcogolloperez13@gmail.com>
2025-10-04 15:06:15 -0500

    Update Login.APP_BasedeDatos.sql

--- Login.APP_BasedeDatos.sql
+++ Login.APP_BasedeDatos.sql
@@ -1,13 +1,1 @@
-CREATE DATABASE IF NOT EXISTS sistema_usuarios;
-USE sistema_usuarios;
 
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    gmail VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    clave VARCHAR(255) NOT NULL
-);
-
-INSERT INTO usuarios (nombre, gmail, username, clave)
-VALUES ('José', 'jose@gmail.com', 'jose1', '123');
