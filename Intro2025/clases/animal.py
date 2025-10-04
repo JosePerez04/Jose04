3ac7c11bd1c3ab4a8fcb142dffb704218384e1a5
JosePerez04 <josedavidcogolloperez13@gmail.com>
2025-10-04 15:04:25 -0500

    Delete Intro2025 directory

--- Intro2025/clases/animal.py
+++
@@ -1,7 +0,0 @@
-class Animal:
-    def __init__(self, nombre, peso):
-        self.nombre = nombre
-        self.peso = peso
-
-    def __str__(self):
-        return f"Nombre: {self.nombre}, Peso: {self.peso} kg"
--- Intro2025/main.py
+++
@@ -1,5 +0,0 @@
-from clases.ave import Ave
-
-if __name__ == "__main__":
-    mi_ave = Ave("Loro", 2.5, 2018, "Jose David Perez")
-    print(mi_ave)
